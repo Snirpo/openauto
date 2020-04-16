265b9261d4a61f5d7b0b5b85518bdc096e206093
Chiel Prins <snirpo@gmail.com>
2020-04-16 23:58:35 +0200

    improvements for macos

--- macos_install.sh
+++ macos_install.sh
@@ -1,2 +1,13 @@
-brew install gstreamer gst-plugins-base gst-plugins-good \
-      gst-plugins-bad gst-plugins-ugly gst-libav \
+#!/bin/zsh
+brew install gstreamer \
+      gst-plugins-base \
+      gst-plugins-good \
+      gst-plugins-bad \
+      gst-plugins-ugly \
+      gst-libav \
+      qt5 \
+      boost \
+      util-linux \
+      rt-audio \
+      libusb \
+      openssl
